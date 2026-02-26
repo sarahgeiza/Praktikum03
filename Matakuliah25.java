8fdaa9455e1d51bb24f3c5968917a0a723276e73
sarahgeiza <sarahgeizask@gmail.com>
2026-02-26 11:09:26 +0700

    Percobaan 3

---
+++ Matakuliah25.java
@@ -0,0 +1,14 @@
+public class Matakuliah25 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah25(String kode, String nama, int sks, int jumlahJam) {
+         this.kode = kode;
+         this.nama = nama;
+         this.sks = sks;
+         this.jumlahJam = jumlahJam;
+    }
+}
+
