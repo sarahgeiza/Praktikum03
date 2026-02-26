e5363e67ded3bc70c44bc19a1ed545a58f0ce9ab
sarahgeiza <sarahgeizask@gmail.com>
2026-02-26 23:25:59 +0700

    Percobaan 3 Pertanyaan 4

--- MatakuliahDemo25.java
+++ MatakuliahDemo25.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 public class MatakuliahDemo25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah25[] arrayOfMatakuliah25 = new Matakuliah25[3];
+
+        System.out.print("Masukkan jumlah Matakuliah : ");
+        int jumlahMatakuliah = Integer.parseInt(sc.nextLine());
+
+        Matakuliah25[] arrayOfMatakuliah25 = new Matakuliah25[jumlahMatakuliah];
+
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for(int i=0; i < 3; i++) {
+        for(int i=0; i < jumlahMatakuliah; i++) {
             System.out.println("Masukkan Daata Matakuliah ke-" + (i + 1));
             System.out.print("Kode          : ");
             kode = sc.nextLine();
@@ -20,10 +25,13 @@ public class MatakuliahDemo25 {
             jumlahJam = Integer.parseInt(dummy);
             System.out.println("--------------------------------------");
 
-            arrayOfMatakuliah25[i] = new  Matakuliah25(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah25[i] = new Matakuliah25(kode, nama, sks, jumlahJam);
             
             arrayOfMatakuliah25[i].tambahData(kode, nama, sks, jumlahJam);
-    
+        }
+        System.out.println();
+        System.out.println("-----Daftar Mata Kuliah-----");
+        for(int i=0; i<jumlahMatakuliah; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah25[i].cetakInfo();
         }
