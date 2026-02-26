7510e0a5a6ce84133f3f50860e7768c52d0fa44f
sarahgeiza <sarahgeizask@gmail.com>
2026-02-26 22:42:35 +0700

    Percobaan 2 Pertanyaan 1

--- Mahasiswa25.java
+++ Mahasiswa25.java
@@ -4,4 +4,11 @@ public class Mahasiswa25 {
     public String kelas;
     public float ipk;
 
+    public void cetakInfo() {
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("---------------------");
+    }
 }
